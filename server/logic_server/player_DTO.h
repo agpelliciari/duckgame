a2cda0cf4a8cb8e6b211157bc79581bf759eba2c
Marcelo Origoni Agustin <marce.origoni@gmail.com>
2024-10-19 17:25:10 -0300

    refactor: primer integracion de rama de logica con la de threads, no compila solo uno temporal agregando las clases

--- server/logic_server/player_DTO.h
+++ server/logic_server/player_DTO.h
@@ -6,19 +6,5 @@
 #ifndef PLAYER_DTO_H
 #define PLAYER_DTO_H
 
-struct PlayerDTO{
-    int id;
-    bool is_alive;
-    int coord_x;
-    int coord_y;
-    TypeWeapon weapon;
-    bool helmet;
-    bool chest_armor;
-    TypeMoveAction move_action;
-
-    PlayerDTO(int id_, bool alive, int x, int y, TypeWeapon w, bool h, bool armor, TypeMoveAction action)
-        : id(id_), is_alive(alive), coord_x(x), coord_y(y), weapon(w), helmet(h), chest_armor(armor), move_action(action) {}
-
-};
 
 #endif //PLAYER_DTO_H
