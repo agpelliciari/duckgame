196cd77c8544d9dfcfd1ed4e50e69020cf544557
Marcelo Origoni Agustin <marce.origoni@gmail.com>
2024-10-19 17:29:19 -0300

    update dtos hablados, esta separado por dtos del match/lobby y del player en si por ahora

--- common/dtos.h
+++ common/dtos.h
@@ -2,14 +2,16 @@
 #define LIB_DTOS_H
 
 #include <cstdint>
-#include <sstream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include "common/dtosplayer.h"
 
-enum LobbyActionType : uint8_t {
-    NEW_LOBBY = 0x16,
-    JOIN_LOBBY = 0x17,
+enum LobbyActionType:uint8_t { 
+    NEW_LOBBY = 0x16, 
+    JOIN_LOBBY = 0x17, 
     STARTED_LOBBY = 0x18,
-    // CANCELED_LOBBY = 0x19
+    //CANCELED_LOBBY = 0x19
 };
 
 struct lobby_action {
@@ -18,83 +20,41 @@ struct lobby_action {
 } __attribute__((packed));
 
 
-enum PlayerActionType : uint8_t {
-    PICK_UP = 3,
+enum MatchStateType:uint8_t { 
+    INICIADA= 0x01, 
+    TERMINADA= 0x02, 
+    CANCELADA= 0x03, 
 };
 
-struct player_action_dto {
-    PlayerActionType type;
-    uint8_t playerind;
-    uint8_t specific_info;
-} __attribute__((packed));
-
-
-// Necesitamos el dto del estado ya que ese es el que reciben los notifiers!
-// Osea la queue del notifier es de estados de partida/ snapshots
-// Igual podemos crear la clase, y que sea solo el duckdto o asi
-// O bueno nose ja a veces es mejor ir de lo mas general !
-// Solo falta el tema del estado de partida si, tipo iniciada, terminada , cancelada. Y el nro de
-// ronda claro, o
-struct coordinates_dto {
-    int x;
-    int y;
-} __attribute__((packed));
-
-enum WeaponType : uint8_t {
-    BAZOOKA_W = 0x01,
-};
-
-struct weapon_dto {
-    WeaponType tipo;
-} __attribute__((packed));
-
-struct armor_dto {
-    uint8_t equipped;
-} __attribute__((packed));
-
-
-struct duck_dto {
-    int id;
-    bool is_alive;
-    coordinates_dto coordinates;
-    weapon_dto weapon;
-    armor_dto helmet;
-    armor_dto chest_armor;
-} __attribute__((packed));
-
-
-enum MatchStateType : uint8_t {
-    INICIADA = 0x01,
-    TERMINADA = 0x02,
-    CANCELADA = 0x03,
-};
-
-struct match_info_dto {
+struct match_info_dto{
     MatchStateType estado;
     uint8_t numronda;
+    //uint8_t cantidadjugadores; 
 };
 
 class MatchDto {
 public:
     match_info_dto info;
-
-    explicit MatchDto(MatchStateType _estado, uint8_t _numronda): info({_estado, _numronda}) {}
-    explicit MatchDto(match_info_dto _info): info(_info) {}
+    std::vector<PlayerDTO> players;
+    
+    explicit MatchDto(MatchStateType _estado, uint8_t _numronda): info({_estado, _numronda}){} 
+    explicit MatchDto(match_info_dto _info): info(_info){}
+    
+    
+    
     std::string parse() const {
-        std::stringstream result;
-        if (info.estado == INICIADA) {
-            result << "INICIADA ";
-        } else if (info.estado == TERMINADA) {
-            result << "TERMINADA ";
-
-        } else if (info.estado == CANCELADA) {
-            result << "CANCELADA ";
-        }
-        result << (int)info.numronda;
-        return result.str();
+          std::stringstream result;
+          if(info.estado == INICIADA){
+               result << "INICIADA ";
+          } else if(info.estado == TERMINADA){
+               result << "TERMINADA ";
+          
+          } else if(info.estado == CANCELADA){
+               result << "CANCELADA ";
+          }
+          result << (int)info.numronda;
+          return result.str();
     }
 };
-
-//
 
 #endif
---
+++ common/dtosplayer.h
@@ -0,0 +1,53 @@
+#ifndef LIB_DTOS_H
+#define LIB_DTOS_H
+
+enum PlayerActionType:uint8_t { 
+    PICK_UP = 3, 
+};
+
+struct PlayerActionDTO {
+    PlayerActionType type;
+    uint8_t playerind;
+    uint8_t specific_info;
+} __attribute__((packed));
+
+
+
+
+enum class TypeWeapon {
+    NONE,
+    GRANADA,
+    BANANA,
+    PEW_PEW_LASER,
+    LASER_RIFLE,
+    AK_47,
+    PISTOLA_DE_DUELOS,
+    PISTOLA_COWBOY,
+    MAGNUM,
+    ESCOPETA,
+    SNIPER
+};
+
+enum class TypeStatePlayer {
+    NONE,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    STAY_DOWN,
+    JUMP,
+    FLAP
+};
+
+struct PlayerDTO{
+    int id;
+    bool is_alive;
+    int coord_x;
+    int coord_y;
+    TypeWeapon weapon;
+    bool helmet;
+    bool chest_armor;
+    TypeStatePlayer state;
+
+    PlayerDTO(int id_, bool alive, int x, int y, TypeWeapon w, bool h, bool armor, TypeMoveAction action)
+        : id(id_), is_alive(alive), coord_x(x), coord_y(y), weapon(w), helmet(h), chest_armor(armor), move_action(action) {}
+
+}__attribute__((packed));
