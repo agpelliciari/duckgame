dfb2fdeb2e4b7611f9c162cbcfa0bc6adf638c4c
Sebastian M. Vintoñuke <mauvinto@gmail.com>
2024-10-22 06:46:27 -0300

    02 => initial menu

--- client/main.cpp
+++ client/main.cpp
@@ -1,39 +1,39 @@
-#include "common/foo.h"
+#include "menu/mainwindow.h"
 
 #include <iostream>
-#include <exception>
+#include <QApplication>
+#include <QStyleFactory>
 
-#include <SDL2pp/SDL2pp.hh>
-#include <SDL2/SDL.h>
+constexpr int ERROR_CODE = 1;
 
-using namespace SDL2pp;
-
-int main() try {
-	// Initialize SDL library
-	SDL sdl(SDL_INIT_VIDEO);
-
-	// Create main window: 640x480 dimensions, resizable, "SDL2pp demo" title
-	Window window("SDL2pp demo",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			640, 480,
-			SDL_WINDOW_RESIZABLE);
-
-	// Create accelerated video renderer with default driver
-	Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	// Clear screen
-	renderer.Clear();
-
-	// Show rendered frame
-	renderer.Present();
-
-	// 5 second delay
-	SDL_Delay(5000);
-
-	// Here all resources are automatically released and library deinitialized
-	return 0;
-} catch (std::exception& e) {
-	// If case of error, print it and exit with error
-	std::cerr << e.what() << std::endl;
-	return 1;
+int main(int argc, char* argv[]) {
+    try {
+        QApplication application(argc, argv);
+		//-------
+		QPalette darkTheme;
+		darkTheme.setColor(QPalette::Window, QColor(53, 53, 53));
+		darkTheme.setColor(QPalette::WindowText, Qt::white);
+		darkTheme.setColor(QPalette::Base, QColor(25, 25, 25));
+		darkTheme.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+		darkTheme.setColor(QPalette::ToolTipBase, Qt::white);
+		darkTheme.setColor(QPalette::ToolTipText, Qt::white);
+		darkTheme.setColor(QPalette::Text, Qt::white);
+		darkTheme.setColor(QPalette::Button, QColor(53, 53, 53));
+		darkTheme.setColor(QPalette::ButtonText, Qt::white);
+		darkTheme.setColor(QPalette::BrightText, Qt::red);
+		darkTheme.setColor(QPalette::Link, QColor(42, 130, 218));
+		darkTheme.setColor(QPalette::Highlight, QColor(42, 130, 218));
+		darkTheme.setColor(QPalette::HighlightedText, Qt::black);
+		application.setPalette(darkTheme);
+		//------
+		MainWindow window;
+		window.show();
+		return application.exec();
+    } catch (const std::exception& err) {
+        std::cerr << "Something went wrong and an exception was caught: " << err.what() << "\n";
+        return ERROR_CODE;
+    } catch (...) {
+        std::cerr << "Something went wrong and an unknown exception was caught.\n";
+        return ERROR_CODE;
+    }
 }
