4660248181a0a30570913d6d36a49c395a609a8d
agpelliciari <apelliciari@fi.uba.ar>
2024-10-25 14:43:02 -0300

    Limpio main y otros archivos basura

--- client/main.cpp
+++ client/main.cpp
@@ -1,32 +1,13 @@
+#include <exception>
+#include <iostream>
+
+#include <SDL2/SDL.h>
+#include <SDL2pp/SDL2pp.hh>
+
 #include "ui_loop.h"
 
-#include <iostream>
-#include <exception>
-
-#include <SDL2pp/SDL2pp.hh>
-#include <SDL2/SDL.h>
-
-using namespace SDL2pp;
-
-int main() try {
-
-	UILoop uiLoop;
-
-	while (uiLoop.isRunning()) {
-        unsigned int frameStart = SDL_GetTicks();
-
-		uiLoop.handleEvent();
-
-		uiLoop.update();
-
-		uiLoop.draw();
-
-        uiLoop.frameDelay(frameStart);
-	}
-	
-	return 0;
-} catch (std::exception& e) {
-	// If case of error, print it and exit with error
-	std::cerr << e.what() << std::endl;
-	return 1;
+int main() try { return 0; } catch (std::exception& e) {
+    // If case of error, print it and exit with error
+    std::cerr << e.what() << std::endl;
+    return 1;
 }
