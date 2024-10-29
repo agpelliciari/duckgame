93c9978d985222c74b90108f1990e34f9abf86c4
AleSchamun <aleschamun123@gmail.com>
2024-10-29 13:14:17 -0300

    feat: logica de salto

--- server/logic_server/coordinates.h
+++ server/logic_server/coordinates.h
@@ -12,15 +12,19 @@ private:
     int actual_x;   // cppcheck-suppress unusedStructMember
     int actual_y;   // cppcheck-suppress unusedStructMember
     int step;       // cppcheck-suppress unusedStructMember
+    float speed_y;  // cppcheck-suppress unusedStructMember
+    float gravity;  // cppcheck-suppress unusedStructMember
+    int time; // cppcheck-suppress unusedStructMember
 
 public:
     Coordinates(int x, int y);
-
     void get_coordinates(int& x, int& y);
-
     void move_right();
     void move_left();
     void jump();
+    void update_position_y();
+    void update_time();
+
 };
 
 
