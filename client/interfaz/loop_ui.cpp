d0fc77d91b3ed2e89fefe1f6655f1d496665c1f1
agpelliciari <apelliciari@fi.uba.ar>
2024-10-30 19:22:32 -0300

    refactor: Agrego clase EventHandler a UILoop

--- client/interfaz/loop_ui.cpp
+++ client/interfaz/loop_ui.cpp
@@ -8,10 +8,9 @@ UILoop::UILoop(ActionListener& dtoSender, SimpleEventListener& _events,
         renderer(window, -1, SDL_RENDERER_ACCELERATED),
         textures(renderer),
         animation(gameContext),
-        sender(dtoSender),
+        eventHandler(dtoSender, gameContext),
         matchDtoQueue(_events),
         lastUpdate(),
-        context(gameContext),
         isRunning_(true) {}
 
 void UILoop::exec() {
@@ -19,7 +18,7 @@ void UILoop::exec() {
         while (isRunning_) {
             unsigned int frameStart = SDL_GetTicks();
 
-            handleEvent();
+            eventHandler.handle(isRunning_);
 
             update();
 
@@ -36,64 +35,6 @@ void UILoop::exec() {
     }
 }
 
-void UILoop::handleEvent() {
-    SDL_Event event;
-    PlayerActionDTO action;
-
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-            isRunning_ = false;
-            return;
-        } else if (event.type == SDL_KEYDOWN) {
-            switch (event.key.keysym.sym) {
-                case SDLK_RIGHT:
-                    action.type = MOVE_RIGHT;
-                    break;
-                case SDLK_LEFT:
-                    action.type = MOVE_LEFT;
-                    break;
-                case SDLK_DOWN:
-                    action.type = STAY_DOWN;
-                    break;
-                case SDLK_SPACE:
-                    action.type = JUMP;
-                    break;
-                case SDLK_ESCAPE:
-                case SDLK_q:
-                    isRunning_ = false;
-                    return;
-
-                    if (context.dualplay) {
-                        case SDLK_d:
-                            action.type = MOVE_RIGHT;
-                            action.playerind = 1;
-                            break;
-                        case SDLK_a:
-                            action.type = MOVE_LEFT;
-                            action.playerind = 1;
-                            break;
-                        case SDLK_s:
-                            action.type = STAY_DOWN;
-                            action.playerind = 1;
-                            break;
-                        case SDLK_w:
-                            action.type = JUMP;
-                            action.playerind = 1;
-                            break;
-                    }
-            }
-        } else if (event.type == SDL_KEYUP) {
-            action.type = NONE;
-            if ((event.key.keysym.sym == SDLK_a) || (event.key.keysym.sym == SDLK_d) ||
-                (event.key.keysym.sym == SDLK_w) || (event.key.keysym.sym == SDLK_s)) {
-                action.playerind = 1;
-            }
-        }
-
-        sender.doaction(action);
-    }
-}
-
 void UILoop::update() {
 
     MatchDto matchUpdate;
@@ -107,17 +48,13 @@ void UILoop::update() {
 
     animation.updateFrame();
 
-    animation.updateSprite(matchUpdate);
+    animation.updateSprite(lastUpdate);
 }
 
 void UILoop::draw() {
     // Clear screen
     renderer.SetDrawColor(255, 255, 255, 255);  // White background
     renderer.Clear();
-
-    // Draw ground
-    renderer.SetDrawColor(0, 0, 0, 255);  // Black color
-    renderer.DrawLine(0, GROUND_Y, SCREEN_WIDTH, GROUND_Y);
 
     for (const PlayerDTO& player: lastUpdate.players) {
         drawPlayer(player);
