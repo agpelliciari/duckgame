3ab145be70aceea2a9258c5c35128ac2ad436c34
Marcelo Origoni Agustin <marce.origoni@gmail.com>
2024-11-03 11:15:39 -0300

    refactor: updated tests to new scheme for protocols

--- tests/testprotocolintegration.cpp
+++ tests/testprotocolintegration.cpp
@@ -7,47 +7,10 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "tests/core/queuesocket.h"
-#include "tests/core/sharedsocket.h"
 #include "tests/core/testermatchdto.h"
 
 using ::testing::InSequence;
 using ::testing::ThrowsMessage;
-/*
-
-static void assertPlayerAreEq(const PlayerDTO& expected, const PlayerDTO& given) {
-
-    ASSERT_EQ(given.id, expected.id) << "Players id are the same";
-    ASSERT_EQ(given.coord_x, expected.coord_x) << "Players x are the same";
-    ASSERT_EQ(given.coord_y, expected.coord_y) << "Players y are the same";
-    ASSERT_EQ(given.weapon, expected.weapon) << "Players weapon are the same";
-    ASSERT_EQ(given.move_action, expected.move_action) << "Players move_action are the same";
-    ASSERT_EQ(given.doing_action, expected.doing_action) << "Players doing_action are the same";
-    ASSERT_EQ(given.is_alive, expected.is_alive) << "Players is alive are the same";
-    ASSERT_EQ(given.helmet, expected.helmet) << "Players helmet are the same";
-    ASSERT_EQ(given.chest_armor, expected.chest_armor) << "Players chest armor are the same";
-    ASSERT_EQ(given.aiming_up, expected.aiming_up) << "Players aiming up are the same";
-}
-static void assertMatchDtoEq(const MatchDto& expected, const MatchDto& given) {
-    ASSERT_EQ(given.info.estado, expected.info.estado) << "State is the same";
-    ASSERT_EQ(given.info.numronda, expected.info.numronda) << "State round number are the same";
-
-    ASSERT_EQ(given.players.size(), expected.players.size()) << "Player counts are the same";
-
-    auto givenPlayer = given.players.begin();
-    int ind = 0;
-    for (auto expPlayer = expected.players.begin(); expPlayer != expected.players.end();) {
-        ind++;
-        PlayerDTO playerExpected = *expPlayer;
-        PlayerDTO playerGiven = *givenPlayer;
-
-        assertPlayerAreEq(playerGiven, playerExpected);
-
-        ++expPlayer;
-        ++givenPlayer;
-    }
-}
-
-*/
 
 TEST(IntegrationProtocolTest, SendReceiveStateNoPlayer) {
 
@@ -56,8 +19,8 @@ TEST(IntegrationProtocolTest, SendReceiveStateNoPlayer) {
     MatchDto state(INICIADA, 2);
 
     // Los protocols se encargan. De liberar el shared socket.
-    ServerProtocol server(new SharedSocket(msgbase));
-    ClientProtocol client(new SharedSocket(msgbase));
+    ServerProtocol server(msgbase);
+    ClientProtocol client(msgbase);
 
     server.sendstate(state);
 
@@ -77,8 +40,8 @@ TEST(IntegrationProtocolTest, SendReceiveState1PlayerMoveLeft) {
 
 
     // Los protocols se encargan. De liberar el shared socket.
-    ServerProtocol server(new SharedSocket(msgbase));
-    ClientProtocol client(new SharedSocket(msgbase));
+    ServerProtocol server(msgbase);
+    ClientProtocol client(msgbase);
 
     server.sendstate(state);
 
@@ -102,8 +65,8 @@ TEST(IntegrationProtocolTest, SendReceiveState2PlayerMoveLeftAndStill) {
 
 
     // Los protocols se encargan. De liberar el shared socket.
-    ServerProtocol server(new SharedSocket(msgbase));
-    ClientProtocol client(new SharedSocket(msgbase));
+    ServerProtocol server(msgbase);
+    ClientProtocol client(msgbase);
 
     server.sendstate(state);
 
